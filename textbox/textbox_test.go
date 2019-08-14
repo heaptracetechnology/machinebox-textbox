65c5901fc958bdf820c96ba7088b2b2bdcb7d435
ht68rohit <rohits@heaptrace.com>
2019-08-14 12:43:03 +0530

    Added test cases

--- textbox/textbox_test.go
+++ textbox/textbox_test.go
@@ -1,3 +1,63 @@
 package textbox
 
-import ()
+import (
+	"bytes"
+	"encoding/json"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"log"
+	"net/http"
+	"net/http/httptest"
+)
+
+var _ = Describe("Text Analyze with invalid param", func() {
+
+	textbox := []byte(`{"status":false}`)
+	requestBody := new(bytes.Buffer)
+	encodeErr := json.NewEncoder(requestBody).Encode(textbox)
+	if encodeErr != nil {
+		log.Fatal(encodeErr)
+	}
+
+	request, err := http.NewRequest("POST", "/textAnalyze", requestBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(TextAnalyze)
+	handler.ServeHTTP(recorder, request)
+
+	Describe("analyze text", func() {
+		Context("analyze", func() {
+			It("Should result http.StatusBadRequest", func() {
+				Expect(http.StatusBadRequest).To(Equal(recorder.Code))
+			})
+		})
+	})
+})
+
+var _ = Describe("Text Analyze", func() {
+
+	textbox := MachineBox{Address: "http://localhost:8080", Text: "I can't wait for @machineboxio to release Textbox; it provides natural language processing and a whole host of other #useful things."}
+	requestBody := new(bytes.Buffer)
+	encodeErr := json.NewEncoder(requestBody).Encode(textbox)
+	if encodeErr != nil {
+		log.Fatal(encodeErr)
+	}
+
+	request, err := http.NewRequest("POST", "/textAnalyze", requestBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(TextAnalyze)
+	handler.ServeHTTP(recorder, request)
+
+	Describe("analyze text", func() {
+		Context("analyze", func() {
+			It("Should result http.StatusBadRequest", func() {
+				Expect(http.StatusBadRequest).To(Equal(recorder.Code))
+			})
+		})
+	})
+})
